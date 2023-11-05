5e0987a3d527a7918b337326cd509f3c0d669f3f
Batuh <200205011@ostimteknik.edu.tr>
2023-11-05 22:57:30 +0300

    TryDoorAnimationV1

--- Assets/Scripts/PlayerDoorOpenClose.cs
+++ Assets/Scripts/PlayerDoorOpenClose.cs
@@ -1,23 +1,46 @@
+using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDoorOpenClose : MonoBehaviour
+public class PlayerDoorOpenClose : NetworkBehaviour
 {
     [SerializeField] public Animator animator;
+
+    [Networked] public NetworkBool isOpenDoor { get; set; }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            animator.SetTrigger("isDoorOpen");
+            //animator.SetTrigger("isDoorOpen");
+            isOpenDoor = true;
+            Rpc_DoorOpenClose(isOpenDoor);
         }
     }
 
+    
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            animator.SetTrigger("isDoorClose");
+            //animator.SetTrigger("isDoorClose");
+            isOpenDoor = false;
+            Rpc_DoorOpenClose(isOpenDoor);
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void Rpc_DoorOpenClose(NetworkBool isOpen, RpcInfo info = default)
+    {
+        if (isOpenDoor)
+        {
+            this.animator.SetTrigger("isDoorOpen");
+        }
+        else
+        {
+            this.animator.SetTrigger("isDoorClose");
         }
     }
 }
