3ead4f643821d397b17f3fa42ac6864922f220c6
Batuh <200205011@ostimteknik.edu.tr>
2023-11-08 17:40:11 +0300

    BodyColorChange

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerData : NetworkBehaviour
 {
     [Networked(OnChanged = nameof(ChangedPlayerName))] public NetworkString<_32> playerName { get; set; }  = null;
+
+    [Networked(OnChanged = nameof(ChangedColorChange))] public Color bodyColor { get; set; }
+
+    [SerializeField] private InputActionReference _triggerPress;
+
+    [SerializeField] GameObject PlayerBody;
 
     public TMP_Text PlayerNametext;
 
@@ -19,6 +27,19 @@ public class PlayerData : NetworkBehaviour
         PlayerNametext.text = playerName.ToString();
 
     }
+
+    private static void ChangedColorChange(Changed<PlayerData> changed)
+    {
+        changed.Behaviour.PlayerBody.GetComponent<MeshRenderer>().material.color = changed.Behaviour.bodyColor;
+    }
+    void Update()
+    {
+        if (_triggerPress.action.ReadValue<float>() > 0.9)
+        {
+            bodyColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+
     [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
     public void Rpc_SetPlayerName(string PlayerName, RpcInfo info = default)
     {
