f6738700e982f0e181283f273744a33ae75d6fa5
UfukApaydin <apaydnufuk@gmail.com>
2023-11-20 12:42:21 +0300

    BallTargetTransform

--- Assets/Scripts/Authority.cs
+++ Assets/Scripts/Authority.cs
@@ -10,7 +10,7 @@ public class Authority : NetworkBehaviour
 
     //[SerializeField] private bool isInteractable = true;
     //public bool IsInteractable => isInteractable;
-    [Networked(OnChanged =nameof(GetHolderCharachter))] public PlayerRef currentHolder { get; set; }
+   // [Networked(OnChanged =nameof(GetHolderCharachter))] public PlayerRef currentHolder { get; set; }
     public bool isGrabbed => Object != null && holdingTransform != null;
     [SerializeField] Transform holdingTransform;
     public override void FixedUpdateNetwork()
@@ -18,23 +18,23 @@ public class Authority : NetworkBehaviour
         if (!Object.HasStateAuthority) return;
 
 
-        if (!isGrabbed) return;
+      //  if (!isGrabbed) return;
                
         
         transform.position = holdingTransform.position;
     }
-    public static void GetHolderCharachter(Changed<Authority> changed)
-    {
-        changed.Behaviour.holdingTransform = changed.Behaviour.FindHolderCharachter(changed.Behaviour.currentHolder);
-    }
+    //public static void GetHolderCharachter(Changed<Authority> changed)
+    //{
+    //    changed.Behaviour.holdingTransform = changed.Behaviour.FindHolderCharachter(changed.Behaviour.currentHolder);
+    //}
     public async void TakeAuthority()
     {
         await Object.WaitforStateAuthority();
         Debug.Log(Object.InputAuthority);
-        if (Object.HasStateAuthority)
-        {
-            currentHolder = Runner.LocalPlayer;
-        }
+        //if (Object.HasStateAuthority)
+        //{
+        //    currentHolder = Runner.LocalPlayer;
+        //}
     }
     [ContextMenu("Grab")]
     public void grab()
@@ -46,12 +46,12 @@ public class Authority : NetworkBehaviour
     {
         TakeAuthority();
     }
-    public Transform FindHolderCharachter(PlayerRef playerRef)
-    {
-        Debug.Log(playerRef);
+    //public Transform FindHolderCharachter(PlayerRef playerRef)
+    //{
+    //    Debug.Log(playerRef);
         
-         return NetworkManager.Instance.SessionRunner.GetPlayerObject(playerRef).transform;
-        Debug.Log(playerRef);
-    }
+    //     return NetworkManager.Instance.SessionRunner.GetPlayerObject(playerRef).transform;
+    //    Debug.Log(playerRef);
+    //}
 
 }
